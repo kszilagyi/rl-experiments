55f8cdca05d2cd5df25e03906b480d88ec192397
kszilagyi <kristof.szilagyi1@gmail.com>
2020-11-08 17:06:38 +0000

    current state

--- cartpole-q-learning.py
+++ cartpole-q-learning.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from model import create_q_model
 
 env = gym.make('CartPole-v0').env
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.05)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.005)
 
 qmodel = create_q_model()
 qmodel_old = tf.keras.models.clone_model(qmodel)
@@ -20,9 +20,8 @@ State = Tuple[float, float, float, float]
 Action = int
 IsTerminal = float
 buffer: List[Tuple[State, Action, float, State, IsTerminal]] = []
-PRINT_FREQ = 100
+PRINT_FREQ = 1
 EPISODE_LEN = 5000
-COPY_FREQ = 5
 EPSILON = 0.1
 GAMMA = 0.95
 np.random.seed(0)
@@ -30,14 +29,12 @@ random.seed(0)
 env.seed(0)
 
 BATCH_SIZE = 256
-MAX_SIZE = 1000
 all_actions = [0, 1]
 def train(i_episode):
-    total_loss = 0
-    total_cnt = 0
-    while total_cnt < 1:
-
-        for _ in range(0, len(buffer), BATCH_SIZE):
+    previous_last_loss = 100
+    for _ in range(20):
+        last_loss = 100
+        while last_loss > 0.1:
             states, actions, rewards, new_states, is_terminal = list(zip(*random.sample(buffer, BATCH_SIZE)))
             left = np.array([[*s, 0] for s in new_states])
             right = np.array([[*s, 1] for s in new_states])
@@ -49,11 +46,20 @@ def train(i_episode):
                 diff = tf.squeeze(q_values) - (rewards + GAMMA * tf.squeeze(best_q_values_from_new_state)) * (1.0 - np.array(is_terminal))
                 loss = tf.reduce_mean(tf.square(diff))
             # if i_episode %10 == 0:
-            #     print(loss.numpy())
-            total_loss += loss.numpy()
-            total_cnt += 1
+            # print('mean: ' + str(np.mean(q_values.numpy())))
+            # print(loss.numpy())
+            if last_loss != 100:
+                previous_last_loss = last_loss
+            last_loss = loss.numpy()
             gradients = tape.gradient(loss, qmodel.trainable_variables)
             optimizer.apply_gradients(zip(gradients, qmodel.trainable_variables))
+        print('COPY')
+        print('mean: ' + str(np.mean(q_values.numpy())))
+        qmodel_old.set_weights(qmodel.get_weights())
+        # if previous_last_loss < 0.1 and last_loss < 0.1:
+        #     print('BREAK')
+        #     break
+        previous_last_loss = last_loss
     # print(f'mean loss: {total_loss/total_cnt}, total cnt: {total_cnt}')
 
 
@@ -72,21 +78,23 @@ def epsilon_greedy_action(state):
 def main():
     global qmodel_old
     global buffer
+    global EPSILON
     max_return = 0
     cum_return = 0
     for i_episode in range(100000):
         state = env.reset()
         episode_return = 0
+        pre_buffer = []
         for t in range(EPISODE_LEN):
-            if i_episode % PRINT_FREQ == 0 and i_episode != 0:
-                env.render()
-                sleep(0.1)
+            # if i_episode % PRINT_FREQ == 0 and i_episode != 0:
+            #     env.render()
+            #     sleep(0.1)
 
             action = epsilon_greedy_action(state)
             new_state, reward, done, info = env.step(action)
             episode_return += reward
-            buffer.append((state, action, reward, new_state, float(done)))
-            buffer = buffer[-MAX_SIZE:]
+            pre_buffer.append((state, action, reward, new_state, float(done)))
+            # buffer = buffer[-MAX_SIZE:]
             # if done:
             #     for i in range(int(t/2)):
             #         buffer.append((state, action, reward, new_state, float(done))) # balancing classes todo do i need this, maybe it was the lr rate after all
@@ -96,13 +104,15 @@ def main():
             if done or t + 1 == EPISODE_LEN:
                 max_return = max(max_return, episode_return)
                 cum_return += episode_return
-                if len(buffer) >= BATCH_SIZE:
-                    train(i_episode)
                 if i_episode % PRINT_FREQ == 0:
-                    print("Episode {} finished after {} timesteps".format(i_episode, t+1))
-                if i_episode % COPY_FREQ == 0:
-                    print('COPY')
-                    qmodel_old.set_weights(qmodel.get_weights())
+                    print("500 - Episode {} finished after {} timesteps, buffer size: {}".format(i_episode, t+1, len(buffer)))
+                if t < 500:
+                    buffer.extend(pre_buffer)
+                if len(buffer) >= BATCH_SIZE*10 and t < 500:
+                    EPSILON = 0
+                    train(i_episode)
+
+
                 if i_episode % 10 == 0:
                     print(f'max return: {max_return}')
                     print(f'avg return: {cum_return / 10}')
