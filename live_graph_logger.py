bd2e48c3979036e70e6797ca16838ba5de7becc4
kszilagyi <kristof.szilagyi1@gmail.com>
2020-11-19 08:16:15 +0000

    graph is a line now

--- live_graph_logger.py
+++ live_graph_logger.py
@@ -1,12 +1,18 @@
+from collections import deque
 from typing import Dict
 
 from environment import LoggerBackend
 import matplotlib.pyplot as plt
 
 class LiveGraphLogger(LoggerBackend):
-    def __init__(self, key, values):
-        self.key = key
+    def __init__(self, x_key, y_key):
+        self.x_key = x_key
+        self.y_key = y_key
+        self.data_x = deque(maxlen=2)
+        self.data_y = deque(maxlen=2)
 
     def log(self, data: Dict[str, float]):
-        plt.scatter(data[self.key], data['episode_return'])
+        self.data_x.append(data[self.x_key])
+        self.data_y.append(data[self.y_key])
+        plt.plot(self.data_x, self.data_y, color='blue', linewidth=1)
         plt.pause(0.05)
--- try.py
+++ try.py
@@ -3,11 +3,14 @@ import gym
 from environment import Environment, Logger
 from live_graph_logger import LiveGraphLogger
 from policy_gradient import PolicyGradient
+
+
 def main():
     episode_length = 100
     algo = PolicyGradient(episode_length=episode_length)
     env = Environment(num_episodes=1000, episode_length=episode_length, env_creator=lambda: gym.make('CartPole-v1'), algo=algo)
-    print(env.train(1, Logger(['episode_return'], LiveGraphLogger('episode_num', None))))
+    print(env.train(1,
+                    Logger(['episode_return'], LiveGraphLogger('episode_num', 'episode_return'))))
 
 
 if __name__ == '__main__':
