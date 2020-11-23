1e0a461463e86320101a4bb1f24b16693ec39bb7
kszilagyi <kristof.szilagyi1@gmail.com>
2020-11-23 08:27:55 +0000

    ./run.sh

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+FILES=$(git status --porcelain)
+if [[ $FILES ]]; then
+  echo "Uncommitted files"
+  echo $FILES
+  exit 1
+fi
+
+VERSION=git rev-parse --short=12 HEAD
+if [[ $(docker image ls | grep $VERSION) ]]; then
+  echo "WARNING!!!!"
+  echo "Tag already exists!"
+  echo "WARNING!!!!"
+  exit 1
+fi
+
+TAG=us.gcr.io/rl-experiments-296208/rl-experiments:$VERSION
+echo "Building $TAG"
+docker build -t "$TAG"
+echo "Pushing"
+docker push $TAG
+echo "Pushed $TAG"
+
