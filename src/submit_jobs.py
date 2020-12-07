9392a6357f0fd6db5750ef7b5dac1f44d6a35171
kszilagyi <kristof.szilagyi1@gmail.com>
2020-12-07 09:23:02 +0000

    better logging

--- src/submit_jobs.py
+++ src/submit_jobs.py
@@ -63,8 +63,9 @@ def main():
         # jobs_ids = [j.name for j in cluster_jobs]
         cluster_pods = v1_core.list_namespaced_pod('default', label_selector=f'batch={batch_name}').items
 
-        logger.info([p for p in cluster_pods]) # todo this is not printing the right thing
+        logger.info('\n'.join([str((p.name, p.status.phase)) for p in cluster_pods])) # todo this is not printing the right thing
         time.sleep(1)
+        print('\n----------------------------\n')
 
 if __name__ == '__main__':
     main()
