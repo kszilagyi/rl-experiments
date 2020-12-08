75fa120aca68283d746729c3a00686dcddb949ad
kszilagyi <kristof.szilagyi1@gmail.com>
2020-12-08 20:31:04 +0000

    policy gradient test v0

--- jobs/test_jobs/policy_gradient_v0.py
+++ jobs/test_jobs/policy_gradient_v0.py
@@ -1,14 +1,13 @@
-name = 'test-job'
+name = 'policy-gradient-test-v0'
 
 static = {
     'algo_path': 'src.policy_gradient',
     'algo_name': 'PolicyGradient',
     'episode_length': 200,
     'environment': 'CartPole-v1',
-    'num_episodes': 10
+    'num_episodes': 2000
 }
 
 grid = {
-    'seed': list(range(2)),
-    'size': ['small', 'large']
+    'seed': list(range(100)),
 }
