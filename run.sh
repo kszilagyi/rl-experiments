a675dad8f7a82c7e91ed7c20910b340d427cade6
kszilagyi <kristof.szilagyi1@gmail.com>
2020-12-08 21:26:11 +0000

    add test mode

--- run.sh
+++ run.sh
@@ -2,15 +2,25 @@
 set -euo pipefail
 
 JOB_SPEC_PATH=$1
-echo "JOB_SPEC_PATH=$JOB_SPEC_PATH"
-FILES=$(git status --porcelain)
-if [[ $FILES ]]; then
-  echo "Uncommitted files"
-  echo $FILES
-  exit 1
+TEST="${2:-}"
+TEST_SUFFIX=''
+if [ "$TEST" = '--test' ]; then
+  TEST_SUFFIX="-test-$(openssl rand -hex 10)"
+  echo '***Test mode***'
 fi
 
-VERSION=$(git rev-parse --short=12 HEAD)
+
+echo "JOB_SPEC_PATH=$JOB_SPEC_PATH"
+if [ ! "$TEST" = '--test' ]; then
+  FILES=$(git status --porcelain)
+  if [[ $FILES ]]; then
+    echo "Uncommitted files"
+    echo $FILES
+    exit 1
+  fi
+fi
+
+VERSION="$(git rev-parse --short=12 HEAD)$TEST_SUFFIX"
 TAG=us.gcr.io/rl-experiments-296208/rl-experiments:$VERSION
 
 if [[ $(docker image ls | grep $VERSION) ]]; then
@@ -25,4 +35,4 @@ else
   echo "Pushed $TAG"
 fi
 
-python -m src.submit_jobs --job_spec_path $JOB_SPEC_PATH --docker_image $TAG
+python -m src.submit_jobs --job_spec_path $JOB_SPEC_PATH --docker_image $TAG $TEST
