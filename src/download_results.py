18f904ee990a40a887c15909d27ccfb7c0e68fff
kszilagyi <kristof.szilagyi1@gmail.com>
2020-12-09 08:50:06 +0000

    download results

---
+++ src/download_results.py
@@ -0,0 +1,22 @@
+import argparse
+
+from google.cloud import storage
+
+from src.logg import logg
+
+logger = logg(__name__)
+
+def main():
+    parser = argparse.ArgumentParser('Run online')
+    parser.add_argument('--batch_name', type=str, required=True)
+    args = parser.parse_args()
+    logger.info(args)
+    batch_name = args.batch_name
+    storage_client = storage.Client()
+    logger.info('Creating job params files')
+    for blob in storage_client.list_blobs('rl-experiments', prefix=batch_name):
+        print(blob.name)
+
+
+if __name__ == '__main__':
+    main()
